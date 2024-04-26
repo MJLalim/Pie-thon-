1269530cfeb43bfbd52dfe5767beeff1899bab63
MJLalim <micheallalim@gmail.com>
2024-04-26 18:31:46 -0400

    keyinterrupt.py not working properly, idea: hit keyboard key to start/stop

---
+++ Automation/Games/keyboardinterrupt.py
@@ -0,0 +1,29 @@
+import time
+import sys
+import os
+import threading
+import keyboard
+import BackEnd
+
+def quitScript():
+    print("QUITTING...")
+    time.sleep(0.2) #wait for the thread to die
+    os.system('stty sane')
+    sys.exit()
+
+def keyBoardInterruptor():
+    try:
+        while True:
+            time.sleep(5)
+            BackEnd.Eternal_Inversionist.allAttack()
+            print("tick")
+            time.sleep(0.5)
+            if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                print('You Pressed A Key!')
+                quitScript()
+    except KeyboardInterrupt:
+        quitScript()
+
+keyBoardInterruptor()
+
+t11t11
